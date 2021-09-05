2687c7281afebce7c1c2f68729dddab1543e0618
luckyshanb4 <luckyshanb4@gmail.com>
2021-09-05 17:55:43 +0530

    create user entity

---
+++ src/main/java/com/Rest/RestAPI/model/User.java
@@ -0,0 +1,35 @@
+package com.Rest.RestAPI.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="users")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name="username",nullable = false)
+	private String username;
+	
+	@Column(name="password",nullable = false)
+	private String password;
+	
+	@Column(name="first_name",nullable = false)
+	private String firstName;
+	
+	@Column(name="last_name",nullable = false)
+	private String lastName;
+	
+	@Column(name="email",nullable = false)
+	private String email;
+
+}
